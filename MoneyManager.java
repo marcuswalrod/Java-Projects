4c19e90586b3afeb6087437cf2b7bd53d3c0d345
marcuswalrod <marcuswalrod13@gmail.com>
2024-03-02 12:44:22 -0500

    Week 2 Programming

--- MoneyManager.java
+++ MoneyManager.java
@@ -1,11 +1,12 @@
 //import java.io.File;
 //import java.io.IOException;
+import java.util.Scanner;
 
 
 public class MoneyManager {
 
     public static void main(String args[]) {
-        
+        Envelope.CreateEnvelope();
     }
 }
     
